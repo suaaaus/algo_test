f6c3a50d72dcbf4cb148b6c1ef2eeb925433ef73
suaaaus <tndk0505@naver.com>
2025-07-10 22:52:21 +0900

    [Silver III] Title: 바이러스, Time: 64 ms, Memory: 34924 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,23 @@
+from collections import deque
+n = int(input())
+m = int(input())
+computers=[[0]*(n+1) for _ in range(n+1)]
+for _ in range(m):
+    a,b = map(int,input().split())
+    computers[a][b] = 1
+    computers[b][a] = 1
+    
+visited = [0]*(n+1)
+q=deque()
+q.append(1)
+visited[1] = 1
+cnt = 0
+
+while q:
+    now = q.popleft()
+    for i in range(1,n+1):
+        if computers[now][i] == 1 and visited[i] == 0:
+            visited[i]=1
+            q.append(i)
+            cnt += 1
+print(cnt)
